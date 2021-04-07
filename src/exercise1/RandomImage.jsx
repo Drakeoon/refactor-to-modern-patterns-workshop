efe746006e1ead6c1c29c12a6fa32eb3b70e6822
Daniel Szczepanik <tmlm18@gmail.com>
2021-04-07 17:35:32 +0200

    Refactor example 1

--- src/exercise1/RandomImage.jsx
+++ src/exercise1/RandomImage.jsx
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { Image } from '@chakra-ui/react';
 
+const fetchRandomImage = async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await response.json();
+  const characters = data.results;
+  const character = characters[Math.floor(Math.random() * characters.length)];
+
+  return character.image;
+};
+
+const useRandomImage = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    async function fetchImage() {
+      const randomImageUrl = await fetchRandomImage();
+
+      setImageUrl(randomImageUrl);
+    }
+
+    fetchImage();
+  }, []);
+
+  return imageUrl;
+};
+
+const UseRandomImage = ({ children }) => {
+  const randomImage = useRandomImage();
+
+  return children(randomImage);
+};
+
 class RandomImage extends React.Component {
-  state = {
-    imageUrl: undefined,
-  };
-
-  componentDidMount = () => {
-    this.fetchRandomImage();
-  };
-
-  fetchRandomImage = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
-    const data = await response.json();
-    const characters = data.results;
-    const character = characters[Math.floor(Math.random() * characters.length)];
-    this.setState({ imageUrl: character.image });
-  };
-
   render() {
-    const { imageUrl } = this.state;
-
-    return <Image src={imageUrl} />;
+    return (
+      <UseRandomImage>{imageUrl => <Image src={imageUrl} />}</UseRandomImage>
+    );
   }
 }
 
 export default RandomImage;
-
-
-
